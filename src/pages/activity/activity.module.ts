01fdcd1d46005075b9d94a9117404722861fc68c
Lakshya Agrawal <lavi061997@gmail.com>
2018-09-15 22:44:10 +0530

    add welcome page

---
+++ src/pages/activity/activity.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { IonicPageModule } from "ionic-angular";
+import { ActivityPage } from "./activity";
+import { PipesModule } from "../../pipes/pipes.module";
+@NgModule({
+  declarations: [ActivityPage],
+  imports: [IonicPageModule.forChild(ActivityPage), PipesModule]
+})
+export class ActivityPageModule {}
