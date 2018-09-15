1789e192723f7c0ee2b6c0e01eb718f5fedccc35
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2018-09-15 09:23:19 +0000

    Add Login UI (#1)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ActivityPage } from '../pages/activity/activity';
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
+import { LoginPage } from "../pages/login/login";
+import { Geolocation } from "@ionic-native/geolocation";
+
+import { ActivityPage } from "../pages/activity/activity";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    ActivityPage
-  ],
+  declarations: [MyApp, HomePage, ActivityPage, LoginPage],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp, {
-      mode: 'ios'
+      mode: "ios"
     })
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    ActivityPage,
-  ],
+  entryComponents: [MyApp, HomePage, ActivityPage, LoginPage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Geolocation,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,19 +1,23 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { ActivityPage } from '../activity/activity';
+import { Component } from "@angular/core";
+import { NavController, ModalController } from "ionic-angular";
+import { LoginPage } from "../login/login";
+import { ActivityPage } from "../activity/activity";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    public modalCtrl: ModalController
+  ) {}
+  login() {
+    let loginpage = this.modalCtrl.create(LoginPage);
+    loginpage.present();
   }
 
   openActivity() {
     this.navCtrl.setRoot(ActivityPage);
   }
-
 }
