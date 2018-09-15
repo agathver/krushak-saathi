230eb3788aee3b17c2e6953049367950c7f42c4a
Lakshya Agrawal <lavi061997@gmail.com>
2018-09-15 13:56:22 +0530

    login changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController, ModalController } from "ionic-angular";
+import { LoginPage } from "../login/login";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    public modalCtrl: ModalController
+  ) {}
+  login() {
+    let loginpage = this.modalCtrl.create(LoginPage);
+    loginpage.present();
   }
-
 }
---
+++ src/pages/login/login.ts
@@ -0,0 +1,26 @@
+import { Component, ViewChild } from "@angular/core";
+import { IonicPage, NavController, NavParams,Slides  } from "ionic-angular";
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: "page-login",
+  templateUrl: "login.html"
+})
+export class LoginPage {
+  @ViewChild(Slides) slides: Slides;
+
+  crops: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    console.log("ionViewDidLoad LoginPage");
+  }
+}
