b03d91e043b98111ee1dd88c1181e7a62e2032e5
Lakshya Agrawal <lavi061997@gmail.com>
2018-09-15 16:30:57 +0530

    Add login css and html

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,16 +2,32 @@ import { Component } from "@angular/core";
 import { NavController, ModalController } from "ionic-angular";
 import { LoginPage } from "../login/login";
 import { ActivityPage } from "../activity/activity";
+import { UserProvider } from "../../providers/user/user";
 
 @Component({
   selector: "page-home",
   templateUrl: "home.html"
 })
 export class HomePage {
+
+  busy:boolean
+
   constructor(
-    public navCtrl: NavController,
-    public modalCtrl: ModalController
-  ) {}
+    private navCtrl: NavController,
+    private modalCtrl: ModalController,
+    private userProvider:UserProvider) {
+  }
+
+  async ngOnInit() {
+    this.busy = true;
+    const user = await this.userProvider.currentUser();
+
+    this.busy = false;
+    if(user) {
+      this.openActivity();
+    }
+  }
+
   login() {
     let loginpage = this.modalCtrl.create(LoginPage);
     loginpage.present();
